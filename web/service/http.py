483c87e33cd47639b0e2ca65ddb1020aa5e8a7db
smilu97 <smilup2244@gmail.com>
2017-08-16 00:11:52 +0900

    fix blob generate signed url

--- web/service/http.py
+++ web/service/http.py
@@ -2,6 +2,7 @@
 
 from ..secret import *
 import urllib
+from datetime import datetime, timedelta
 
 def wget(remote_path, save_path):
 	urllib.urlretrieve(remote_path, save_path)
@@ -15,4 +16,4 @@ bucket = client.get_bucket(FIREBASE_BUCKET)
 def upload_cloud(blobname, filename):
 	blob = bucket.blob(blobname)
 	blob.upload_from_filename(filename=filename)
-	return blob.generate_signed_url()
+	return blob.generate_signed_url(datetime.now() + timedelta(365))
